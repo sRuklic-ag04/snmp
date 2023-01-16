ebf70fa7c8c477f69f0109c5abb8924cc6d5f1d5
sRuklic-ag04 <stjepan.ruklic@wearenotch.com>
2023-01-16 17:55:51 +0100

    Initial project commit

---
+++ snmp/src/test/java/com/kontron/snmp/SnmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kontron.snmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
